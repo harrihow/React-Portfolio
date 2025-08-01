348757dfb5bf2629ee882fb953d3829ae1b52418
harrifano1997 <richanne.harri@gmail.com>
2025-08-02 07:02:13 +0800

    Added ProjectSection component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
 import React from 'react'
-
-
 import Navbar from './components/Nav/Navbar.jsx'
 import HeroSection from './components/HeroSection/HeroSection.jsx'
-/*
-
 import ProjectSection from './components/ProjectSection/ProjectSection.jsx'
-*/
-
 
 export default function App() {
   return (
     <>
       <Navbar />
       <HeroSection />
-    </>
-    /*
-    
       <ProjectSection />
-  */
+    </>
   );
 }
 
