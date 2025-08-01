978f8f3f157d733825344cf5c333814a126c06f6
harrifano1997 <richanne.harri@gmail.com>
2025-08-02 06:39:18 +0800

    Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
+
+
 import Navbar from './components/Nav/Navbar.jsx'
+/*
 import HeroSection from './components/HeroSection/HeroSection.jsx'
 import ProjectSection from './components/ProjectSection/ProjectSection.jsx'
+*/
+
 
 export default function App() {
   return (
-    <div>Hello</div>
-    /*
+  
     <>
       <Navbar />
-      <HeroSection />
-      <ProjectSection />
     </>
+    /*
+    <HeroSection />
+      <ProjectSection />
   */
   );
 }
