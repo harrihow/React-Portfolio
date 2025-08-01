e063c7dd7524d4ff80872201dd5c90498c0e2ba2
harrifano1997 <richanne.harri@gmail.com>
2025-08-02 06:23:24 +0800

    Tetst

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import Navbar          from './components/Nav/Navbar.jsx'
-import HeroSection     from './components/HeroSection/HeroSection.jsx'
-import ProjectSection  from './components/ProjectSection/ProjectSection.jsx'
+import Navbar from './components/Nav/Navbar.jsx'
+import HeroSection from './components/HeroSection/HeroSection.jsx'
+import ProjectSection from './components/ProjectSection/ProjectSection.jsx'
 
 export default function App() {
   return (
+    <div>Hello</div>
+    /*
     <>
       <Navbar />
       <HeroSection />
       <ProjectSection />
     </>
-  
+  */
   );
 }
 
