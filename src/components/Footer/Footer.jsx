4fbfa53c9ae569150f4f674b97d4f7e08e7bef99
harrifano1997 <richanne.harri@gmail.com>
2025-08-02 23:17:08 +0800

    Added footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,12 @@
-
-import React from 'react'
-import styles from '../ContactSection/ContactSection.module.css'
+import React from 'react';
+import styles from './Footer.module.css';
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className="container">
-        <p>© 2024 Harri Fano. Built with HTML, CSS and passion.</p>
+        <p className={styles.text}>&copy; {new Date().getFullYear()} HF. All rights reserved.</p>
       </div>
     </footer>
-  )
+  );
 }
